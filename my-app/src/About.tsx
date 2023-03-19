20cd7ffb87894b50e1bc470695484168828e0d59
jkeegan1 <joshjkeegan@gmail.com>
2023-03-18 23:35:57 -0400

    added drawer to about page

--- my-app/src/About.tsx
+++ my-app/src/About.tsx
@@ -1,5 +1,9 @@
+import Drawer from './Drawer'
 export default function About(){
     return (
-        <></>
-    )
+      <>
+      <Drawer></Drawer>
+        <p> Now in the about page</p>
+      </>
+    );
 }
