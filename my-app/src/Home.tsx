d31e3653fcee2f1d6fddef1bf40a06a222afc9e4
jkeegan1 <joshjkeegan@gmail.com>
2023-03-18 23:36:17 -0400

    added drawer to home page

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -1,6 +1,12 @@
+import Drawer from './Drawer'
 export default function Home() {
+
     return (
-    <></>
+    <>
+    <Drawer></Drawer>
+    <h1>Home page header</h1>
+    <p> now at the home page</p>
+    </>
 
     )
 }
