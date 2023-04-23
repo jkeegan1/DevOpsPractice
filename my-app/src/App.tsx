d105c5fbdba458cd0c0d982dccc865b237d998d9
jkeegan1 <joshjkeegan@gmail.com>
2023-04-23 16:07:20 -0400

    updates

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Drawer from './Drawer'
 import Home from './Home'
 import About from './About'
+import Login from "./Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles.css";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/Home" element={<Home></Home>} />
           <Route path="/About" element={<About></About>} />
+          <Route path="/Login" element={<Login></Login>} />
         </Routes>
       </BrowserRouter>
       <div className="App"></div>
---
+++ my-app/src/Login.tsx
@@ -0,0 +1,104 @@
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import InputLabel from "@mui/material/InputLabel";
+import React from "react";
+import FormControl from "@mui/material/FormControl";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Typography from "@mui/material/Typography";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Login() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+  return (
+    <Card
+      sx={{
+        width: "80vh",
+        height: "60vh",
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+        ml: "30%",
+        mt: "10%",
+        flexDirection: "column",
+      }}
+    >
+      <Box sx={{ ml: "30%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            flexDirection: "column",
+            width: "50%",
+            gap: 4,
+          }}
+        >
+          <AccountCircle
+            sx={{
+              width: "15vw",
+              height: "15vh",
+              display: "flex",
+            }}
+          ></AccountCircle>
+          <Box sx={{ ml: "20%" }}>
+            <Typography variant="h3" component="h3">
+              Sign in
+            </Typography>
+          </Box>
+          <TextField label="Username" id="UnInput" />
+          <FormControl sx={{}} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        </Box>
+      </Box>
+    </Card>
+  );
+}
